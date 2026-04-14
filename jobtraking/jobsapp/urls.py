df3c35344277436fbeb0dc7ff9a7a3ac513cda22
MeghaJ <www.meghaj80@gmail.com>
2026-04-14 21:37:17 +0530

    Update files

--- jobtraking/jobsapp/urls.py
+++ jobtraking/jobsapp/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('add/', views.add_job, name='add_job'),
     path('delete/<int:id>/', views.delete_job, name='delete_job'),
+    path('jobs/', views.jobs_list, name='jobs'),
+    path('applications/', views.applications, name='applications'),
+    path('apply-job/<int:id>/', views.apply_job, name='apply_job'),
+    path('delete-application/<int:id>/', views.delete_application, name='delete_application'),
+
 ]
--- jobtraking/jobsapp/views.py
+++ jobtraking/jobsapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Job
+
 # Create your views here.
 
 
@@ -19,3 +20,40 @@ def add_job(request):
 def delete_job(request, id):
     Job.objects.filter(id=id).delete()
     return redirect('dashboard')
+
+def jobs_list(request):
+    jobs = Job.objects.all()
+    return render(request, 'jobs.html', {'jobs': jobs})
+
+
+def applications(request):
+    if not request.session.get('user_id'):
+        return redirect('login')
+
+    jobs = Job.objects.filter(user_id=request.session.get('user_id'))
+    return render(request, 'application.html', {'jobs': jobs})
+def apply_job(request, id):
+    if not request.session.get('user_id'):
+        return redirect('login')
+
+    job = Job.objects.filter(id=id).first()
+
+    if not job:
+        return redirect('jobs')
+
+    job.status = "Applied"
+    job.user_id = request.session.get('user_id')
+    job.save()
+
+    return redirect('applications')
+
+def delete_application(request, id):
+    if not request.session.get('user_id'):
+        return redirect('login')
+
+    job = Job.objects.filter(id=id, user_id=request.session.get('user_id')).first()
+
+    if job:
+        job.delete()   # removes application
+
+    return redirect('applications')
